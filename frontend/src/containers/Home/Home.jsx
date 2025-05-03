6881180b4536279ad7ee7d3b64876199e50122d5
Tanay Kumar <tanaykmrdev@gmail.com>
2025-05-03 17:37:14 +0530

    test2 commit

--- frontend/src/containers/Home/Home.jsx
+++ frontend/src/containers/Home/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="text-center">
       <main>
-        <h1 className="mt-4 mb-2">Welcome to the Home Page!</h1>
+        <h1 className="mt-4 mb-2">Wellcome to the Home Page!</h1>
         <p>Believe you can and you're halfway there.</p>
       </main>
       <nav>
